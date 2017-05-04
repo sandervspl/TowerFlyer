4777d6f8ff148457147fcfe815cc78af31a1a3bc
Sander Vispoel <sandervispoel@hotmail.com>
2017-05-04 20:53:22 +0200

    Added decorators support. Updated TSLint.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log('hello world!');
+const isACar = (target) => {
+  target.isACar = true;
+};
+
+@isACar
+class Car {
+  private brand: string;
+  public static isACar: boolean;
+
+  constructor (brand: string) {
+    this.brand = brand;
+  }
+
+  public getBrand = (): string => this.brand;
+}
+
+const car = new Car('Ferrari');
+
+console.log(`${car.getBrand()} is a car: ${Car.isACar}`);
