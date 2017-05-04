cbbabdba6441e2e38bec06b50f7004c45890e199
Sander Vispoel <sandervispoel@hotmail.com>
2017-05-04 20:31:08 +0200

    Webpack update

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -3,9 +3,9 @@ const base = require('./base.config.js');
 const path = require('path');
 
 module.exports = _.assign({}, base, {
+  devtool: 'eval',
   devServer: {
     contentBase: path.resolve(__dirname, '..', 'public'),
-    // hot: true,
     inline: true
   },
 })
