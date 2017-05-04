25893ac6a53b5d6fec41afdb055b135078df8c45
Sander Vispoel <sandervispoel@hotmail.com>
2017-05-04 21:16:09 +0200

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
-  console.log('PRODUCTION');
   module.exports = require('./webpack/prod.config.js')
 } else {
-  console.log('DEV SERVER STARTING');
   module.exports = require('./webpack/dev.config.js')
 }
