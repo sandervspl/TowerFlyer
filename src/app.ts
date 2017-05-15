11bee9d2c19cbaec04ea7d89d5c6ec68f295df64
Sander Vispoel <sandervispoel@hotmail.com>
2017-05-15 12:51:22 +0200

    resize renderer view on browser resize

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 // dependencies
 import { Application } from 'pixi.js';
 import Game from './game/Game';
+import _ from 'lodash';
 
 class App {
   private static pixiApp: Application;
@@ -15,8 +16,20 @@ class App {
     // add canvas element to DOM
     document.body.appendChild(App.pixiApp.view);
 
+    // add event listeners
+    this.addEventListeners();
+
     // init game
     new Game();
+  }
+
+  private addEventListeners = () => {
+    window.addEventListener('resize', _.debounce((e) => {
+      e.preventDefault();
+
+      App.pixiApp.renderer.view.width = window.innerWidth;
+      App.pixiApp.renderer.view.height = window.innerHeight;
+    }, 500));
   }
 
   public static getView = ():Application => App.pixiApp;
