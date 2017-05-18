f04bafb63c145866b2c8631d4c983b563db9f5be
Sander Vispoel <sandervispoel@hotmail.com>
2017-05-18 19:59:58 +0200

    preloader update

--- src/game/Preloader.ts
+++ src/game/Preloader.ts
@@ -1,14 +1,17 @@
 class Preloader {
   private container;
   private loader;
+  private progress;
 
   constructor() {
     this.container = document.querySelector('#pre-loader');
     this.loader = document.querySelector('#pre-loader .loader .inner');
+    this.progress = document.querySelector('#pre-loader #progress');
   }
 
   public update = (progress: number): void => {
     this.loader.style.width = `${progress}%`;
+    this.progress.innerHTML = `${progress}%`;
   }
 
   public end = (): void => {
